3ee0fe8140a0c990bd1dc377b2f95189e56e921d
Mirmik <mirmikns@yandex.ru>
2017-02-10 11:39:48 +0300

    a

---
+++ arch/avr/include/halgxx/irqs_lock.h
@@ -0,0 +1,19 @@
+#ifndef GXX_AVR_irqs_lock_H
+#define GXX_AVR_irqs_lock_H
+
+#include <hal/irq.h>
+#include <inttypes.h>
+
+namespace gxx {
+	class irqs_lock {
+
+	static uint8_t __irqs_lock_counter;
+	static irqstate_t __irqs_lock_save;
+
+	public:
+		void lock();
+		void unlock();
+	};
+}
+
+#endif
