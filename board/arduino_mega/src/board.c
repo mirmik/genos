ab6d5d8800a67da44ccb7e43553da33031868ce1
Mirmik <mirmikns@yandex.ru>
2017-03-09 01:27:10 +0300

    a

--- board/arduino_mega/src/board.c
+++ board/arduino_mega/src/board.c
@@ -3,6 +3,7 @@
 #include <hal/arch.h>
 #include <hal/irq.h>
 #include <debug/delay.h>
+#include <debug/dprint.h>
 
 void board_init() {
 	arch_init();
@@ -23,6 +24,7 @@ void board_shutdown(arch_shutdown_mode_t mode) {
 		case ARCH_SHUTDOWN_MODE_ABORT:
 			global_irq_disable();
 			gpio_settings(GPIOB, (1<<7), GPIO_MODE_OUTPUT);
+			debug_print("arch_shutdown"); dln();
 			while(1) {
 				pin_tgl_level(RED_LED);
 				debug_delay(100);
