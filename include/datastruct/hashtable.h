e8df88216fd4cda535460b3b868dc7c0e9f9fe0f
Mirmik <mirmikns@yandex.ru>
2017-03-14 18:24:21 +0300

    a

--- include/datastruct/hashtable.h
+++ include/datastruct/hashtable.h
@@ -69,13 +69,48 @@ void debug_print_hashtable(struct hashtable* ht) {
 
 __END_DECLS
 
-#define hashtable_get_typed(ht,key,type,lnk) \
+#define hashtable_get_entry(ht,key,type,lnk) \
 	mcast_out_or_null(hashtable_get(ht,key),type,lnk)
 
-#define __hashtable_for_each(pos,ht,head) array_for_each(head,(ht)->table,(ht)->tblsize) hlist_for_each(pos,head)
+/*#define __hashtable_for_each(pos,ht,head) array_for_each(head,(ht)->table,(ht)->tblsize) hlist_for_each(pos,head)
 #define hashtable_for_each(pos,ht) __hashtable_for_each(pos,ht,MACRO_GUARD(head))
 
 #define __hashtable_for_each_entry(pos,ht,lnk,head) array_for_each(head,(ht)->table,(ht)->tblsize) hlist_for_each_entry(pos,head,lnk)
 #define hashtable_for_each_entry(pos,ht,lnk) __hashtable_for_each_entry(pos,ht,lnk,MACRO_GUARD(head))
+*/
+
+static inline struct hlist_head* ht_next_nempty_head(struct hlist_head* head, struct hlist_head* hend) {
+	for(;head != (hend);head++) if (head->first != NULL) return head; 				
+	return NULL;					
+}
+
+static inline struct hlist_head* __hashtable_for_each_initializer(struct hlist_head* head, struct hlist_head* hend, struct hlist_node** pos) {
+	head = ht_next_nempty_head(head, hend);
+	*pos = head == NULL ? NULL : (head)->first;
+	return head;
+}
+
+#define __hashtable_for_each(pos,head,ht,hend) \
+	for(struct hlist_head *hend = (ht)->table + (ht)->tblsize, *head = __hashtable_for_each_initializer((ht)->table, hend, &pos); \
+		pos != NULL; \
+		pos = pos->next == NULL ? ({head = ht_next_nempty_head(head + 1, hend); head == NULL ? NULL : (head)->first;}) : (pos)->next)
+
+#define hashtable_for_each(pos,ht) __hashtable_for_each(pos,MACRO_GUARD(head),ht,MACRO_GUARD(hend))
+
+
+#define ____hashtable_for_each_entry_initializer(table,hend,pos,hd,lnk) ({	\
+	struct hlist_head* hd = ht_next_nempty_head(table,hend);					\
+	pos = hlist_first_entry(hd, decltypeof(*pos), lnk);	\
+	hd;	\
+})
+#define __hashtable_for_each_entry_initializer(table,hend,pos,lnk) \
+	____hashtable_for_each_entry_initializer(table,hend,pos,MACRO_GUARD(head),lnk)
+
+#define __hashtable_for_each_entry(pos,head,ht,hend,lnk) \
+	for(struct hlist_head *hend = (ht)->table + (ht)->tblsize, *head = __hashtable_for_each_entry_initializer((ht)->table,hend,pos,lnk); \
+		pos != NULL; \
+		pos = &hlist_next_entry(pos,lnk)->lnk == NULL ? ({head = ht_next_nempty_head(head + 1, hend); head == NULL ? NULL : hlist_first_entry(head,decltypeof(*pos),lnk);}) : hlist_next_entry(pos,lnk))
+
+#define hashtable_for_each_entry(pos,ht,lnk) __hashtable_for_each_entry(pos,MACRO_GUARD(head),ht,MACRO_GUARD(hend),lnk)
 
 #endif
