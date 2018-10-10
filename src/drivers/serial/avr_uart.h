21c2519cc29d04e03c33e688295750e562891644
mirmik <mirmikns@yandex.ru>
2018-10-10 09:02:04 +0300

    a

--- src/drivers/serial/avr_uart.h
+++ src/drivers/serial/avr_uart.h
@@ -4,11 +4,46 @@
 #include <drivers/serial/uart.h>
 #include <periph/regs/usart.h>
 
+#include <hal/irqtable.h>
+
 struct avr_uart {
 	struct uart u;
-
+	struct uart_regs * regs;
+	uint8_t base_irq;
 };
 
-int avr_uart_init(struct avr_uart * dev, )
+int avr_uart_enable(struct uart * dev, bool en) {
+	
+}
+
+int avr_uart_getc(struct uart *dev) {
+	
+}
+
+int avr_uart_putc(struct uart *dev, int symbol, bool last) {
+	
+}
+
+int avr_uart_hasrx(struct uart *dev) {
+	
+}
+
+int avr_uart_setup(struct uart *dev, const struct uart_params *params) {
+	
+}
+
+const uart_operations avr_uart_operations = {
+	.enable = avr_uart_enable,
+	.getc = avr_uart_getc,
+	.putc = avr_uart_putc,
+	.hasrx = avr_uart_hasrx,
+	.setup = avr_uart_setup
+}
+
+int avr_uart_init(struct avr_uart * dev, struct uart_regs * regs, int base_irq) {
+	dev->regs = regs;
+	dev->base_irq = base_irq;
+	dev->u->u_op = &avr_uart_operations;
+};
 
 #endif
