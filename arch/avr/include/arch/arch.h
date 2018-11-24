79a78dd6b130b2c81d51280ea42ba1efd932b08f
mirmik <mirmikns@yandex.ru>
2018-11-24 23:37:35 +0300

    a

--- arch/avr/include/arch/arch.h
+++ arch/avr/include/arch/arch.h
@@ -2,12 +2,4 @@
 # error "Do not include this file directly!"
 #endif /* HAL_ARCH_H_ */
 
-#include <util/endian.h>
-
-#ifndef __BYTE_ORDER
-#define __BYTE_ORDER __LITTLE_ENDIAN
-#endif
-
-#define BYTE_ORDER    __BYTE_ORDER
-
 #define PLATFORM_ARCH "avr"
