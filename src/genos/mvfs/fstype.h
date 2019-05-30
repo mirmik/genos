96d47f9c1645cf326852f51511df0fb385ee1134
Mirmik <mirmikns@yandex.ru>
2019-05-30 18:21:37 +0300

    a

--- src/genos/mvfs/fstype.h
+++ src/genos/mvfs/fstype.h
@@ -3,12 +3,14 @@
 
 namespace genos 
 {
-	struct slist_head lnk;
-	const char * name;
-	const struct file_system_operations * fs_ops;
+	class file_system_type 
+	{
+		struct slist_head lnk;
+		const char * name;
 
-	virtual struct super_block * get_sb(struct file_system_type*, int, const void *) = 0;	
-	virtual void kill_sb(struct file_system_type*, struct super_block *) = 0;
+		virtual genos::super_block * get_sb(struct file_system_type*, int, const void *) = 0;	
+		virtual void kill_sb(struct file_system_type*, struct super_block *) = 0;
+	}
 }
 
 #endif
