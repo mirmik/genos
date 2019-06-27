47f8ecadda53471ba32431167932b221248ca97c
mirmik <mirmikns@yandex.ru>
2019-06-27 18:47:27 +0300

    'a'

--- src/drivers/button/button_checker.h
+++ src/drivers/button/button_checker.h
@@ -6,6 +6,11 @@
 
 #include <systime/systime.h>
 
+#define BUTTON_PRESS_EVENT 1
+#define BUTTON_LONG_PRESS_EVENT 2
+#define BUTTON_RELEASE_EVENT 3
+#define BUTTON_NONE 0
+
 // Простой драйвер кнопки. Не использует прерывания.
 // Учитывает дребезг контактов.
 // Расчитывает время нажатия.
@@ -17,12 +22,26 @@ namespace genos
 		incremental_schmidt schmidt;
 		const genos::pin pin;
 
-		bool last = true;
-		time_t lastchange = 0;
+		time_t presstime = 0;
+		time_t longtime = 300;
+
+		bool unpressed_level;
 
 	public:
-		button_checker(const genos::pin pin) :
-			schmidt(10, 20, 30, last), pin(pin)
+		union
+		{
+			uint8_t sts;
+			struct
+			{
+				uint8_t press : 1;
+				uint8_t longpress : 1;
+			};
+		};
+
+	public:
+		button_checker(const genos::pin pin, uint8_t unpressed_level) :
+			schmidt(10, 20, 30, unpressed_level), pin(pin), 
+			unpressed_level(unpressed_level)
 		{}
 
 		int check()
@@ -30,12 +49,36 @@ namespace genos
 			int val = pin.get();
 			bool ret = schmidt.check(val ? +1 : -1);
 
-			if (ret != last) 
+			if (unpressed_level) 
+				ret = !ret;
+
+			if (ret == true)
 			{
-				lastchange = millis();
+				if (press == 0)
+				{
+					press = 1;
+					presstime = millis();
+					return BUTTON_PRESS_EVENT;
+				}
+
+				if (press && !longpress && millis() - presstime > longtime)
+				{
+					longpress = 1;
+					return BUTTON_LONG_PRESS_EVENT;
+				}
 			}
 
-			return ret;
+			else 
+			{
+				if (press == 1) 
+				{
+					press = 0;
+					longpress = 0;
+					return BUTTON_RELEASE_EVENT;
+				}
+			}
+
+			return BUTTON_NONE;
 		}
 	};
 }
