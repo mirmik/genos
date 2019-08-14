1f689d75532ca350c10578dc86a63795866c85d7
mirmik <mirmikns@yandex.ru>
2019-08-14 12:03:54 +0300

    m

--- src/genos/cdev.h
+++ src/genos/cdev.h
@@ -4,6 +4,8 @@
 #include <genos/dev.h>
 #include <defs/ioflags.h>
 
+#include <igris/util/numconvert.h>
+
 namespace genos 
 {
 	class char_device : public genos::named_node
@@ -14,6 +16,27 @@ namespace genos
 		{
 			devmngr.link(this);
 		} 
+
+		int print(const char * str, int flags) 
+		{ 
+			return write(str, strlen(str), flags); 
+		}
+
+		int print(int32_t arg, int flags) 
+		{ 
+			char buf[32];
+			char* eptr = i32toa(arg, buf, 10);
+			return write(buf, eptr - buf, flags); 
+		}
+
+		template<typename T>
+		int println(const T& arg, int flags) 
+		{
+			int ret = 0;
+			ret += print(arg,flags);
+			ret += print("\r\n",flags);
+			return ret;
+		}	
 	};
 }
 
