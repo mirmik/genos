95cc538de8af09fb764e93c159702731e0dcd8db
nfsorokin <sorokin@smitek-robotics.com>
2024-12-24 14:57:20 +0300

    'clean'

--- rfdaemon/src/version.h
+++ rfdaemon/src/version.h
@@ -2,11 +2,12 @@
 #define VERSION_H
 
 const int API_VERSION = 250;
-const std::string VERSION = "1.1.0";
+const std::string VERSION = "1.1.1";
 
 
 // 200 - добавлена поддержка systemd
 // 250 / 1.0.0 - рефактор легаси сервера
 // 250 / 1.1.0 - фикс бага с некорректным обновлением статуса проектов в systemd
+// 250 / 1.1.1 - фикс утечки файловых дескрипторов
 
 #endif
